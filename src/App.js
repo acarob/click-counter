2eaecdd4c78d5b4d59f2d4ad101b0dd4eb339224
Andrea Barrera <andrea.barrera7@gmail.com>
2023-02-22 23:04:35 -0500

    Styles were changed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import "./App.css";
-import freeCodeCampLogo from "./images/FreeCodeCamp_logo.png";
+// import freeCodeCampLogo from "./images/FreeCodeCamp_logo.png";
 import Button from "./components/Button";
 import Counter from "./components/Counter";
+import { useState } from "react";
 
 function App() {
+  const [clickNum, setClickNum] = useState(0);
+
   const manageClick = () => {
-    console.log("Click");
+    setClickNum(clickNum + 1);
   };
 
   const resetCounter = () => {
-    console.log("Reset");
+    setClickNum(0);
   };
 
   return (
     <div className="App">
       <div className="freecodecamp-logo-container">
-        <img
+        <h1 className="neon">Click Counter</h1>
+        {/* <img
           className="freecodecamp-logo"
           src={freeCodeCampLogo}
           alt="freecodecamp logo"
-        />
+        /> */}
       </div>
       <div className="main-container">
-        <Counter clickNum="5"></Counter>
+        <Counter clickNum={clickNum}></Counter>
         <Button text="Click" isClickButton={true} manageClick={manageClick} />
         <Button text="Reset" isClickButton={false} manageClick={resetCounter} />
       </div>
